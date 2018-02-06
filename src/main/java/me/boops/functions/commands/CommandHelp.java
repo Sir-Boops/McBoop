5ba606488d1f98c7310f0f13e7995135508553db
Sir_Boops <admin@boops.me>
2018-02-06 13:22:13 -0700

    Reorder the help command page

--- src/main/java/me/boops/functions/commands/CommandHelp.java
+++ src/main/java/me/boops/functions/commands/CommandHelp.java
@@ -14,6 +14,8 @@ public class CommandHelp {
 		System.out.println("");
 		System.out.println("McBoop --account <user name> => Used with `--run` to run MC with a non-default account");
 		System.out.println("");
+		System.out.println("McBoop --remove-account <username> => Remove the account assoiced with <username>");
+		System.out.println("");
 		System.out.println("McBoop --run <MC version ID> => launches the disred MC version ( You can get a MC id using --list-all-versions )");
 		System.out.println("");
 		System.out.println("McBoop --list-all-versions => Lists all the MC versions you can run");
@@ -26,20 +28,18 @@ public class CommandHelp {
 		System.out.println("");
 		System.out.println("McBoop --update-profile-forge <profile name> <version> => Update a profile to use X version");
 		System.out.println("");
-		System.out.println("McBoop --profile <profile nmae> => Launch said profile");
+		System.out.println("McBoop --delete-profile <profile name> => Deletes a profile from disk");
+		System.out.println("");
+		System.out.println("McBoop --profile <profile name> => Launch said profile");
+		System.out.println("");
+		System.out.println("McBoop --list-profiles => List all profiles and what version they launch");
 		System.out.println("");
 		System.out.println("McBoop --list-forge-versions [MC version] => [MC Version is optional] Without this it will list all versions of MC that forge can on on");
 		System.out.println(" With the argument it will list only forge versions for the requested version!");
 		System.out.println("");
 		System.out.println("McBoop --with-forge <version id> => Used with --run to launch MC with the requested forge version");
 		System.out.println("");
-		System.out.println("McBoop --remove-account <username> => Remove the account assoiced with <username>");
-		System.out.println("");
-		System.out.println("McBoop --list-profiles => List all profiles and what version they launch");
-		System.out.println("");
 		System.out.println("McBoop --max-ram <amount> => Set the max amount of ram to use Default: 2G");
-		System.out.println("");
-		System.out.println("McBoop --delete-profile <profile name> => Deletes a profile from disk");
 		System.out.println("");
 		
 		System.out.println("");
