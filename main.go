7c322403dbe7af3ed74e8d41c5255f2ed7a4a264
Sir_Boops <admin@boops.me>
2019-02-12 09:50:41 -0700

    Bump now installs and checks java installs

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-  fmt.Println("Hewwo!")
+func main() () {
+
+  // TODO: Add support for windows
+
+  // Init folder check
+  // if $HOME/.mcboop is not there create it
+  // Has tailing slash attached already!
+  mcboop_home_path := init_folder_check() // Returns: mcboop_home_path
+  fmt.Println(mcboop_home_path)
+
+  // Check the java install
+  java_bin := check_java_install(mcboop_home_path) // Returns java bin
+  fmt.Println(java_bin)
+
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # McBoop
 
 Golang rewrite
+Requires dep
