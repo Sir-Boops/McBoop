ed6ae49c97dc2aed8752b63618daf2fa5b634c1a
Sir_Boops <admin@boops.me>
2019-06-13 10:01:19 -0600

    Add auto updating

--- launcherupdate.go
+++ launcherupdate.go
@@ -1,5 +1,21 @@
 package main
 
+import "os"
+import "fmt"
+import "path/filepath"
+
 func LauncherUpdate() {
-  // TODO: This.
+
+  expath, _ := filepath.Abs(os.Args[0])
+  local_sum := Sha256Sum(expath)
+  remote_sum := GetRemoteText("https://boops-deploy.s3.amazonaws.com/McBoop/McBoop.sha256")
+
+  if local_sum != remote_sum {
+    fmt.Println("A launcher update has been found and will now be downloaded")
+    WriteFile(ReadRemote("https://boops-deploy.s3.amazonaws.com/McBoop/McBoop"), expath)
+    fmt.Println("Update done, re-run your last command and enjoy! :)")
+    os.Exit(0)
+  }
+
+  fmt.Println(remote_sum, expath)
 }
