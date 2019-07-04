d00fb792a7d5ea1dabce1e3c34b9e2842f01b7e1
Sir_Boops <admin@boops.me>
2019-07-04 08:52:54 -0600

    Force file deletion on windows

--- launcher.go
+++ launcher.go
@@ -2,6 +2,8 @@ package main
 
 import "os"
 import "fmt"
+import "os/exec"
+import "runtime"
 import "path/filepath"
 
 func LauncherUpdate() {
@@ -12,6 +14,10 @@ func LauncherUpdate() {
 
   if local_sum != remote_sum {
     fmt.Println("A launcher update has been found and will now be downloaded")
+    if runtime.GOOS == "windows" {
+      cmd := exec.Command("cmd", "/C", "del", "/f", expath)
+      cmd.Run()
+    }
     WriteFile(ReadRemote("https://boops-deploy.s3.amazonaws.com/McBoop/" + filepath.Base(expath)), expath)
     fmt.Println("Update done, re-run your last command and enjoy! :)")
     os.Exit(0)
