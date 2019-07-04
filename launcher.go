be6919c5313943775e87f37495ed41eda5758dd8
Sir_Boops <admin@boops.me>
2019-07-04 09:08:24 -0600

    Rename then replace?

--- launcher.go
+++ launcher.go
@@ -15,8 +15,7 @@ func LauncherUpdate() {
   if local_sum != remote_sum {
     fmt.Println("A launcher update has been found and will now be downloaded")
     if runtime.GOOS == "windows" {
-      cmd := exec.Command("cmd", "/C", "del", "/f", expath)
-      cmd.Run()
+      os.Rename(expath, expath + ".old")
     }
     WriteFile(ReadRemote("https://boops-deploy.s3.amazonaws.com/McBoop/" + filepath.Base(expath)), expath)
     fmt.Println("Update done, re-run your last command and enjoy! :)")
